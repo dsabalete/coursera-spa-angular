9a8e2f430cc02f07d68435ecd583e283eacaaa47
David Sabalete <dsabalete@gmail.com>
2016-09-12 23:34:42 +0200

    Add module1 solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,9 +1,40 @@
 (function () {
     'use strict';
 
-    angular.module('LunchChecker', [])
-    
-    .controller('LunchCheckerController', function ($scope) {
+    angular.module('LunchCheck', [])
 
-    });
+        .controller('LunchCheckController', LunchCheckController);
+
+    LunchCheckController.$inject = ['$scope'];
+
+    function LunchCheckController ($scope) {
+        $scope.menu = '';
+        $scope.message = '';
+        $scope.numItems = 0;
+
+        $scope.check = function () {
+            var items = $scope.menu.split(',');
+
+            if ($scope.menu.trim() === '') {
+                $scope.message = 'Please enter data first';
+                return;
+            }
+
+            var result = [];
+            angular.forEach(items, function (item) {
+                if (item !== '') {
+                    result.push(item);
+                }
+            });
+
+            $scope.numItems = result.length;
+
+            if (result.length <= 3) {
+                $scope.message = 'Enjoy!';
+            } else {
+                $scope.message = 'Too much!';
+            }
+        };
+    }
+
 })();
